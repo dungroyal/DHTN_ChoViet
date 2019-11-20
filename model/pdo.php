387080718e564769dc3227b4e22e6468fc284b74
Tuedmps08672 <daominhtue200@gmail.com>
2019-11-20 15:40:09 +0700

    đổi lại tên database nha

--- model/pdo.php
+++ model/pdo.php
@@ -3,7 +3,7 @@
  * Mở kết nối đến CSDL sử dụng PDO
  */
 function pdo_get_connection(){
-    $dburl = "mysql:host=localhost;dbname=choviet;charset=utf8";
+    $dburl = "mysql:host=localhost;dbname=1234;charset=utf8";
     $username = 'root';
     $password = '';
 
