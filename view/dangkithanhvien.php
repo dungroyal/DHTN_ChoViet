dc843131ce13bd6cf815441bcf0c7f472d785714
Tuedmps08672 <daominhtue200@gmail.com>
2019-11-29 00:28:07 +0700

    ,

--- view/dangkithanhvien.php
+++ view/dangkithanhvien.php
@@ -1,7 +1,7 @@
 
 
             <div class="signup-form-dangki">
-<<<<<<< HEAD
+
                 <div class="signup-form-header-dangki">Đăng ký nhà cung cấp</div>
                
                 <div class="checkin-dangki">
@@ -43,52 +43,8 @@
                     <button>Đăng kí</button>
                     <button>Hủy bỏ</button>
                 </div>
-=======
-                <form action="#" method="post">
-                    <div class="signup-form-header-dangki">Đăng ký nhà cung cấp</div>
-                
-                    <div class="checkin-dangki">
-                        <div>Số điện thoại liên hệ (&#42;):</div>
-                    
-                        <input type="text"  />
-                    </div>
-                    <div class="checkin-dangki">
-                        <div>Tên liên hệ (&#42;):</div>
-                    
-                        <input type="text"  />
-                    </div>
-                    <div class="checkin-dangki">
-                        <div>Tên shop (&#42;):</div>
-                    
-                        <input type="text"  />
-                    </div>
-                    <div class="checkin-dangki">
-                        <div>Ngày đăng kí (&#42;):</div>
-                    
-                        <input type="date" name="bday"  />
-                    </div>
-                    <div class="checkin-dangki">
-                        <div>Email liên hệ (&#42;):</div>
-                    
-                        <input type="text"  />
-                    </div>
-                    <div class="checkin-dangki">
-                        <div>Mật khẩu (&#42;):</div>
-                    
-                        <input type="password"  />
-                    </div>
-                    <div class="checkin-dangki">
-                        <div>Nhập lại mật khẩu (&#42;):</div>
-                        
-                        <input type="password" />
-                    </div>
-                    <div class="signup-form-button">
-                        <input type="submit" name="add-store" value="Gửi thông tin đăng kí">
-                        <input type="reset" value="Nhập lại">
-                    </div>
-                </form>
->>>>>>> abed3aa1b677a9da781bee9c1cde4d4adf53586e
 
+              
             </div>
 
       
